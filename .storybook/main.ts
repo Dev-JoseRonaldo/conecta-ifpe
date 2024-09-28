b98eca94e129be123004dbce2d4307afcfe34399
José Ronaldo <dev.joseronaldo@gmail.com>
2024-09-28 12:36:23 -0300

    fix: adjuste storybook file config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,10 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
-  stories: ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*stories.@(js|jsx|ts|tsx)",
+  ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",

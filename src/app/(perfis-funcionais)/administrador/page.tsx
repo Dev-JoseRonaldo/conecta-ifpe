122690bba85ad4500a2f24f2989972091906bb7d
José Ronaldo <dev.joseronaldo@gmail.com>
2024-10-06 20:17:20 -0300

    feat: connect requests admin page to backend

--- src/app/(perfis-funcionais)/administrador/page.tsx
+++ src/app/(perfis-funcionais)/administrador/page.tsx
@@ -1,66 +1,108 @@
-// Tela principal logado como Administrador
 "use client"
 
+import axios from "axios"
 import { useEffect, useState } from "react"
 import Button from "src/components/Button"
+import LoadingSpinner from "src/components/LoadingSpinner"
 import Table from "src/components/Table"
-import mockData from "./mock.json"
-
 import RootLayout from "../layout"
 
 interface UsuarioProps {
-  Nome: string
-  CPF: string
-  DataNascimento: string
-  Sexo: string
-  Login: string
-  Senha: string
-  Email: string
-  Campus: string
-  Telefone: string
-  Role: string
-  RoleDesejada: string
+  username: string
+  password: string
+  role: string
+  desiredRole: string
+  siape?: string
+  fullName: string
+  cpf: string
+  birthDate: string
+  email: string
+  campus: string
+  phone: string
 }
 
-export default function AnalysisAssistenteSocialPage() {
+export default function RequestsAdminPage() {
   const [isMounted, setIsMounted] = useState(false)
   const [responseData, setResponseData] = useState<UsuarioProps[]>([])
+  const [selectedUsers, setSelectedUsers] = useState<UsuarioProps[]>([])
 
   const userRole = "Admin"
 
   useEffect(() => {
     setIsMounted(true)
-
-    const fetchData = async () => {
-      setResponseData(mockData)
-    }
     fetchData()
   }, [])
 
+  // Mova a função fetchData para fora do useEffect
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users?role=CONVIDADO", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      setResponseData(response.data)
+    } catch (error) {
+      console.error("Erro ao buscar usuários:", error)
+    }
+  }
+
+  const handleCheckboxSelection = (selectedIds: string[]) => {
+    const selected = responseData.filter((user) => selectedIds.includes(user.cpf))
+    setSelectedUsers(selected)
+  }
+
+  const handleConfirm = async () => {
+    if (selectedUsers.length === 0) {
+      alert("Selecione pelo menos um usuário para confirmar.")
+      return
+    }
+
+    try {
+      await axios.patch("http://localhost:3000/users/update-multiple", selectedUsers, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      alert("Usuários atualizados com sucesso.")
+
+      await fetchData()
+      setSelectedUsers([])
+    } catch (error) {
+      console.error("Erro ao atualizar usuários:", error)
+      alert("Erro ao atualizar usuários.")
+    }
+  }
+
   // Renderiza o layout apenas depois que o componente foi montado no cliente
   if (!isMounted) {
-    return null // Ou pode retornar um spinner ou qualquer placeholder
+    return <LoadingSpinner />
   }
 
   return (
     <RootLayout userRole={userRole}>
       <div className="rounded-lg bg-white p-8 shadow-md">
-        <div className="flex flex-col items-center space-y-4">
-          <h1 className="mb-6 self-start text-2xl font-bold">Solicitações:</h1>
-          <div className="max-h-dvh overflow-scroll">
-            <Table type="Lista de pagamento" IdentifierColumn="CPF" alunos={responseData} colunasOmitidas={["Senha"]} />
-          </div>
-        </div>
-        <div className="mt-8 flex w-full items-center justify-center gap-10">
-          <Button
-            text="Confirmar"
-            color="green"
-            size="content"
-            className="px-8"
-            // onClick={handleGenerateList}
-            // disabled={!selectedFile || progress < 100}
-          />
-        </div>
+        {responseData?.length === 0 ? (
+          <p>Nenhum solicitação pendente.</p>
+        ) : (
+          <>
+            <div className="flex flex-col items-center space-y-4">
+              <h1 className="mb-6 self-start text-2xl font-bold">Solicitações:</h1>
+              <div className="max-h-dvh overflow-scroll">
+                <Table
+                  type="Lista de pagamento"
+                  IdentifierColumn="cpf"
+                  alunos={responseData}
+                  colunasOmitidas={["senha", "id"]}
+                  onCheckboxSelection={handleCheckboxSelection}
+                />
+              </div>
+            </div>
+            <div className="mt-8 flex w-full items-center justify-center gap-10">
+              <Button text="Confirmar" color="green" size="content" className="px-8" onClick={handleConfirm} />
+            </div>
+          </>
+        )}
       </div>
     </RootLayout>
   )
